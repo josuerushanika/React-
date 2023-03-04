569c234005170bf4b5031aa486cd341019ccddea
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-04 06:05:45 -0800

    Initialize repo with some commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        header
+      </header>
+      <div>
+        Main Content
+      </div>
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
