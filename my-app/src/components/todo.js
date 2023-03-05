45c2a9107eca7185990756b857e712e9601c58dc
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-05 04:18:40 -0800

    Set a value and update it on a onther place

--- my-app/src/components/todo.js
+++ my-app/src/components/todo.js
@@ -2,9 +2,16 @@
 import React, {useState} from "react";
 
 const Todo = () => {
-    const [name,setName] = useState('Rushanika');
+    const [name,setName] = useState('');
+
+    const changeName =(value) => {
+       setName(value);
+    }
+
     return (
         <>
+        <input type="text" placeholder="Enter your name" value={name} onChange= {(event) => changeName(event.target.value)}/>
+        <br/>
            Name:{name}
         </>
     
